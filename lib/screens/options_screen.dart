f9e9616cfa1644c685d3a66d3efa840e9cc9c247
Cristofer <ctorrejoncortes1996@gmail.com>
2022-10-07 16:39:44 -0300

    agregate imagen del demonio

--- lib/screens/options_screen.dart
+++ lib/screens/options_screen.dart
@@ -6,14 +6,31 @@ class OptionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Opciones"),
-        elevation: 5,
-        backgroundColor: const Color.fromRGBO(8, 75, 129, 10),
-      ),
-      body: const Center(
-        child: Text('OptionScreen'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Opciones"),
+          elevation: 5,
+          backgroundColor: const Color.fromRGBO(8, 75, 129, 10),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Image.asset(
+              'assets/nodisponible.jpg',
+              fit: BoxFit.cover,
+            ),
+            const Divider(
+              height: 5,
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                  text: 'Próximamente en desarrollo.',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ));
   }
 }
